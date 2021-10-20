730d83ecabd43d77f964111da6d3839c981443e8
Shreyans1602 <shreyans.contact@gmail.com>
2021-10-20 16:28:56 -0500

    Corrected error string spell

--- def.h
+++ def.h
@@ -7,7 +7,7 @@
  */
 
 //Definitions.
-#define ERROR_MSG "An error has occured\n"  //The one and only error message of the program.
+#define ERROR_MSG "An error has occurred\n"  //The one and only error message of the program.
 #define DEFAULT_PATH "/bin"     //Path variable.
 #define DEFAULT_CMD_BUF_SIZE 1024   //Size of command buffer.
 
